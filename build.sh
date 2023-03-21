90c42663f983227a2b57a045b1561351049a8e79
Lucian Popescu <lucian.popescu187@gmail.com>
2023-03-21 09:48:03 +0200

    compile with my stock clang 15.0.7

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -ex
 
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" -S ./llvm -B build/
+# Compile with my clang
+CC=/ssd/llvm-project-master/build/bin/clang CXX=/ssd/llvm-project-master/build/bin/clang++ PATH=/ssd/llvm-project-master/build/bin/clang:$PATH \
+	cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_PROJECTS="llvm;clang" -S ./llvm -B build/
 ninja -C build/
