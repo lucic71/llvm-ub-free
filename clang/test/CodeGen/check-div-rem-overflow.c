a3f06e47e450800cabc953d676c4e1fec2a90243
lucic71 <lucian.popescu187@gmail.com>
2023-07-02 12:29:28 +0300

    Add rem test for check-div-rem-overflow

--- clang/test/CodeGen/check-div-rem-overflow.c
+++ clang/test/CodeGen/check-div-rem-overflow.c
@@ -17,3 +17,21 @@ int f(int x, int y) {
 
   return x / y;
 }
+
+int g(int x, int y) {
+  // CHECK: %[[B0:.*]] = icmp eq i32 %[[D:.*]], 0
+  // CHECK: %[[B1:.*]] = icmp eq i32 %[[N:.*]], -2147483648
+  // CHECK: %[[B2:.*]] = icmp eq i32 %[[D]], -1
+  // CHECK: %[[B3:.*]] = and i1 %[[B1]], %[[B2]]
+  // CHECK: %[[B4:.*]] = or i1 %[[B0]], %[[B3]]
+  // CHECK: br i1 %[[B4]], label %[[L2:[0-9a-z_.]*]], label %[[L1:[0-9a-z_.]*]]
+
+  // CHECK: {{^|:}}[[L1]]
+  // CHECK-NEXT: srem i32 %[[N]], %[[D]]
+
+  // CHECK: {{^|:}}[[L2]]
+  // CHECK-NEXT: call void @llvm.trap()
+  // CHECK-NEXT: unreachable
+
+  return x % y;
+}
